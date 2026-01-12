2d7ce38887baa7f7092503ba721012a4055904d5
Shreyansh jain <118683295+shreyjain2003@users.noreply.github.com>
2026-01-12 14:39:03 +0530

    Required changes made

--- OrderProcessingApp/Program.cs
+++ OrderProcessingApp/Program.cs
@@ -7,25 +7,25 @@ namespace OrderProcessingApp
         static void Main()
         {
             /// Sample products
-            var p1 = new Product(1, "Laptop", 90000);
-            var p2 = new Product(2, "Mouse", 900);
-            var p3 = new Product(3, "Keyboard", 2000);
-            var p4 = new Product(4, "Monitor", 50000);
-            var p5 = new Product(5, "Headset", 5000);
+            var prod1 = new Product(1, "Laptop", 90000);
+            var prod2 = new Product(2, "Mouse", 900);
+            var prod3 = new Product(3, "Keyboard", 2000);
+            var prod4 = new Product(4, "Monitor", 50000);
+            var prod5 = new Product(5, "Headset", 5000);
 
             /// Sample customers
-            var c1 = new Customer(1, "Shrey");
-            var c2 = new Customer(2, "Tushar");
-            var c3 = new Customer(3, "Apurav");
+            var cust1 = new Customer(1, "Shrey");
+            var cust2 = new Customer(2, "Tushar");
+            var cust3 = new Customer(3, "Apurav");
 
             /// Orders
-            var o1 = new Order(101, c1);
-            o1.AddItem(new OrderItem(p1, 1));
-            o1.AddItem(new OrderItem(p2, 2));
+            var order1 = new Order(101, cust1);
+            order1.AddItem(new OrderItem(prod1, 1));
+            order1.AddItem(new OrderItem(prod2, 2));
 
-            var o2 = new Order(102, c2);
-            o2.AddItem(new OrderItem(p3, 1));
-            o2.AddItem(new OrderItem(p5, 1));
+            var order2 = new Order(102, cust2);
+            order2.AddItem(new OrderItem(prod3, 1));
+            order2.AddItem(new OrderItem(prod5, 1));
 
             var service = new OrderService();
 
@@ -33,8 +33,8 @@ namespace OrderProcessingApp
             service.StatusChanged += Notifications.NotifyCustomer;
             service.StatusChanged += Notifications.NotifyLogistics;
 
-            service.AddOrder(o1);
-            service.AddOrder(o2);
+            service.AddOrder(order1);
+            service.AddOrder(order2);
 
             service.UpdateStatus(101, OrderStatus.Paid);
             service.UpdateStatus(101, OrderStatus.Packed);
